7691e205975ee4a49f63bb68be35443a3705eced
NoorTushar <tushar.noor.khan@gmail.com>
2023-12-10 13:41:48 +0600

    creating-main-folder

---
+++ 1-Arrow-Functions/script.js
@@ -0,0 +1,44 @@
+//traditional function
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 7));
+
+//arrow function
+const sum2 = (a, b) => {
+  return a + b;
+};
+console.log(sum2(5, 7));
+
+//if you have one parameter and also one like of execution, you can shorten the code like below:
+const greet = (user) => user;
+
+//here note that we did not use () as well as we did not use the 'return' keyword as well
+
+//Arrow Functions with ForEach loop
+
+let myArray = ["apple", "orange", "coconut"];
+
+//traditional way:
+myArray.forEach(function (item) {
+  console.log(item);
+});
+
+//using arrow function
+myArray.forEach((item) => console.log(item));
+
+//also
+myArray.forEach((item, index) => {
+  console.log(`Item is: ${item}`);
+  console.log(`at index: ${index}`);
+});
+
+//Arrow Functions with SetTimeout()
+
+//using traditional way
+setTimeout(function () {
+  console.log(`traditional way`);
+}, 2000);
+
+//using arrow function
+setTimeout(() => console.log(`using arrow fn`), 2000);
